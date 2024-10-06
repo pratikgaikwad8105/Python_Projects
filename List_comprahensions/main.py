437e601bdbf63030b3d12f159be186ab6d2b8ac8
Pratik Gaikwad <109135415+pratikgaikwad8105@users.noreply.github.com>
2024-10-06 20:43:27 +0530

    Delete List_comprahensions directory

--- List_comprahensions/main.py
+++
@@ -1,7 +0,0 @@
-# new_list = [n * 2 for n in range(1, 5)]
-# print(new_list)
-
-names = ["Pratik", "Gaikwad", "John", "Deb"]
-
-long_names = [name.upper() for name in names if len(name) > 5]
-print(long_names)
